6179b1b6364af9574ce98f3cce57e1dd5b371614
Aleksandra Mech <olamech2001@wp.pl>
2023-12-06 17:26:23 +0100

    page update the with the chart

--- plotting_data.py
+++ plotting_data.py
@@ -93,36 +93,10 @@ def plotting():
                     print('data2: ',data2)   
 
            
-            #plt.plot(data,data2)
-           # plt.show()
-           # time.sleep(3)
+            plt.plot(data2,data)
+            plt.show()
+            time.sleep(3)
 
 plotting()
 
-                #plt.cla()
-
-                         #plt.plot(x, y1, label='Channel 1')
-                         #plt.plot(x, y2, label='Channel 2')
-
-                         #plt.legend(loc='upper left')
-                        # plt.tight_layout()
-                        
-
-   # data = pd.read_csv('data.csv')
-  #  x = data['x_value']
-   # y1 = data['total_1']
-  #  y2 = data['total_2']
-
-   # plt.cla()
-
-    #plt.plot(x, y1, label='Channel 1')
-    #plt.plot(x, y2, label='Channel 2')
-
-    #plt.legend(loc='upper left')
-    #plt.tight_layout()
-
-
-#ani = FuncAnimation(plt.gcf(), animate, interval=1000, save_count=MAX_FRAMES) #gcf= get current figure ; 1000=1sekunda
-
-#plt.tight_layout()
-#plt.show()
+                
--- website/views.py
+++ website/views.py
@@ -69,73 +69,55 @@ def graph():
         if conn != None:
             cur = conn.cursor()
             otrzymane = "SELECT json_info -> 'values' as keyvalues FROM measurements" 
-            ##sprobowac za zapytania zrobic liste
             cur.execute(otrzymane)
-            # ll =  cur.fetchall()
             conn.commit()
             value = [] 
             converted_value = []
-             #zamienic krotki na liste i pozniej iterowac po liscie # tuple
-            # print('cur:', ll)
             for(values) in cur:
-                #print(values) ##(' 78',)
                 value.append(values)
-                print(value) ##[(' 56',)]
-            for(values) in cur:
-                #print(values) ##(' 78',)
-                value.append(values)
-                print(value) ##[(' 56',)]
 
             query = 'SELECT measurements_date FROM measurements'
             cur.execute(query)
             conn.commit()
-            data3 = [] 
+            x = [] 
+            converted_value = []
             for(measurements_date) in cur:
-                zmienna = str(measurements_date)
-                data2.append(zmienna)
-                print(zmienna)
-            print('data2: ',data2)    
-
+                x.append(measurements_date)
             cur.close()
-            conn.close()     
-            print('lista',value)
+            conn.close()       
+
             data =[]
-            #data3 =[]
-            
+            data2 =[]
             for i in value: 
-                    print('i:',i)
                     to_convert = re.findall(r'\d\d+', str(i)) ##jak zmieniac wartosci w tym nawiasie???
-                   # converted = float(to_convert[0])
+                  #  converted = float(to_convert[0])
                     converted = str(to_convert[0])
-                    print('converted:', converted)
-                    #rand_val = (str(converted))
-                
+                    print('converted:', converted)        
                     data.append(converted)
                     print('data:', data)
-                   # y = str(datetime.datetime.now())
-                   # data3.append(y)
+  
+            for ii in x: 
+                    to_convert2 = re.findall(r'\d+', str(ii)) ##jak zmieniac wartosci w tym nawiasie???
+                  #  converted = float(to_convert[0])
+                    year = str(to_convert2[0])
+                    month = str(to_convert2[1])
+                    day = str(to_convert2[2])
+                    hour = str(to_convert2[3])
+                    minute = str(to_convert2[4])
+                    second = str(to_convert2[5])
+                  #  print('year:', year)  
+                  #  print('month:', month)  
+                  #  print('day:', day)  
+                   # print('hour:', hour) 
+                  #  print('minute:', minute)  
+                   # print('second:', second)   
+                    y = (hour+':'+minute+':'+second)   
+                    measure_day = (day+'-'+month+'-'+year)
+                    print('y: ',y,'measure day: ', measure_day) 
+                    data2.append(y) 
+                    print('data2: ',data2) 
 
-            data2 = [
-                ("01-01-2020", 15),
-                ("01-01-2020", 13),
-                ("01-01-2020", 14),
-                ("01-01-2020", 13),
-                ("01-01-2020", 17),
-                ("01-01-2020", 13),
-                ("01-01-2020", 14),
-                ("01-01-2020", 13),
-                ("01-01-2020", 17),
-                ("01-01-2020", 13),
-                ("01-01-2020", 14),
-                ("01-01-2020", 13),
-                ("01-01-2020", 17),
-                ("01-01-2020", 13),
-                ("01-01-2020", 14),
-                ("01-01-2020", 13),
-                ("01-01-2020", 70),
-                    ]
-
-            labels = [row[0] for row in data3]
+            labels = [row[0] for row in data2]
             values = [row[1] for row in data]
 
             
