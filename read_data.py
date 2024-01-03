991e8735895c3553919afa7f690083bac2da6378
Aleksandra Mech <olamech2001@wp.pl>
2024-01-03 19:07:33 +0100

    deleting unnecessary files

--- read_data.py
+++
@@ -1,8 +0,0 @@
-import serial
-# zeby zobaczyć dostepne porty to w terminalu: python -m serial.tools.list_ports
-ser = serial.Serial(port='COM3',baudrate=9600)
-
-while True:
-    value= ser.readline()
-    valueInString=str(value,'UTF-8')
-    print(valueInString)
